344c5aedd2bd02b4de1a65f999eeab98b8933696
Chandan <chandan.srm05@gmail.com>
2022-07-17 17:46:47 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3c8a22b99amshab4b89b0025b451p116c03jsne76690b4107a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "9332b5edfd39d5cc2f6c4521224fb9e4";
